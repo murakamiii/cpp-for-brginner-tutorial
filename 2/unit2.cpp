48b9dd320eb24bcae00b907eb7222c159bb5e58f
test <9khc727ru7@gmail.com>
2019-03-07 23:49:57 +0900

    unit10

--- 2/unit2.cpp
+++ 2/unit2.cpp
@@ -6,4 +6,8 @@ int main() {
     // unit9
     std::cout << 5 * 10 / 2 << "\n";
     std::cout << 2100000000LL * 210000000LL << "\n";
+
+    // unit10
+    std::cout << 10 / 3 << "\n";
+    std::cout << 10.0 / 3.0 << "\n";
 }
