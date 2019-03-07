56ac978df04d473bfb5705e154f6cbbbe6625f83
test <9khc727ru7@gmail.com>
2019-03-07 23:20:35 +0900

    改行

--- 1/unit1.cpp
+++ 1/unit1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout <<"ぼくはプラス。";
-    std::cout <<"ぼくはプラス。";
-    std::cout <<"ぼくはプラス。";
+    std::cout <<"ぼくはプラス。\n";
+    std::cout <<"ぼくはプラス。\n\n";
+    std::cout <<"ぼくはプラス。\n";
 }
