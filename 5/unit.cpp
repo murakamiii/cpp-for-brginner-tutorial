d7bbf3ff4a39920f609c6fb6ddc9791342390291
murakami <9khc727ru7@gmail.com>
2019-03-09 00:04:54 +0900

    5章

---
+++ 5/unit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() 
+{
+    // unit25
+    int hp = 100;
+    hp -= 20;
+    std::cout << hp << "\n";
+    hp += 10;
+    std::cout << hp << "\n";
+
+    // unit26
+    int atk = 10;
+    atk *= 20;
+    std::cout << atk << "\n";
+    atk /= 4;
+    std::cout << atk << "\n";
+
+    // unit27
+    int def = 5;
+    ++def;
+    std::cout << def << "\n";
+    --def;
+    std::cout << def << "\n";
+
+}
