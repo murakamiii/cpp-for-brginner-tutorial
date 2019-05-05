f20ef3ad1bb291ac4e7a387def093f9661bd890f
murakami <9khc727ru7@gmail.com>
2019-05-06 07:25:21 +0900

    fix cpplint errors

--- dp/intro-dp.cpp
+++ dp/intro-dp.cpp
@@ -1,3 +1,5 @@
+// Copyright 2019 Murakami
+
 /*
   https://mieruca-ai.com/ai/introduction-dynamic-programming/
 */
@@ -10,8 +12,7 @@
 #include <unordered_map>
 #include <numeric>
 
-struct DpEdge
-{
+struct DpEdge {
   std::vector<int> nodes;
   int cost;
   bool isChecked;
@@ -19,8 +20,7 @@ struct DpEdge
   DpEdge(std::vector<int> n, int c): nodes(n), cost(c), isChecked(false){}
 };
 
-struct DpNode
-{
+struct DpNode {
   int cost;
   bool isMinimized;
 
@@ -28,11 +28,10 @@ struct DpNode
 };
 
 template <typename T>
-struct VectorWrapper
-{
+struct VectorWrapper {
   std::vector<T> raw_value;
 
-  VectorWrapper(std::vector<T> vec): raw_value(vec){}
+  explicit VectorWrapper(std::vector<T> vec): raw_value(vec) {}
 
   VectorWrapper filter(std::function<bool(T)> f) {
     std::vector<T> filtered;
@@ -40,8 +39,7 @@ struct VectorWrapper
       raw_value.begin(),
       raw_value.end(),
       back_inserter(filtered),
-      f
-    );
+      f);
 
     return VectorWrapper(filtered);
   }
@@ -51,29 +49,26 @@ struct VectorWrapper
     std::vector<U> transformed;
     std::transform(
       raw_value.begin(),
-      raw_value.end(), 
+      raw_value.end(),
       std::back_inserter(transformed),
-      f
-    );
+      f);
     return VectorWrapper<U>(transformed);
   }
 
   template <typename V>
-  V reduce(V init, std::function<V(V,T)> f) {
+  V reduce(V init, std::function<V(V, T)> f) {
     return std::accumulate(
       raw_value.begin(),
-      raw_value.end(), 
+      raw_value.end(),
       init,
-      f
-    );
+      f);
   }
 
   void foreach(std::function<void(T)> f) {
     std::for_each(
       raw_value.begin(),
       raw_value.end(),
-      f
-    );
+      f);
   }
 };
 
@@ -81,19 +76,18 @@ struct VectorWrapper
 /*
   g++ dp/intro-dp.cpp -std=c++17 -Wall --pedantic-errors && ./a.out dp/eg1.csv
  */
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   std::string fileName = argv[1];
   std::ifstream ifs(fileName);
   if (ifs.fail()) {
     std::cout << "読み込みエラー" << "\n";
   }
-  
+
   std::vector<DpEdge> edges;
   std::unordered_map<int, DpNode> nodes;
 
   std::string line;
-  while(getline(ifs, line)){
+  while (getline(ifs, line)) {
     std::vector<int> v;
     std::stringstream ss{line};
     std::string buf;
@@ -104,30 +98,31 @@ int main(int argc, char const *argv[])
     std::vector<int> node_vec {v[0], v[1]};
 
     edges.push_back(
-      DpEdge(node_vec, v[2])
-    );
+      DpEdge(node_vec, v[2]));
 
     VectorWrapper(node_vec)
       .filter([=](int x) { return nodes.find(x) == nodes.end();})
-      .foreach([&](int key) { 
+      .foreach([&](int key) {
         nodes[key] = DpNode();
       });
   }
-  
+
   int target_node = 0;
   nodes[target_node].isMinimized = true;
   nodes[target_node].cost = 0;
 
-  // while (std::all_of(nodes.begin(), nodes.end(), [](DpNode n) { return n.isMinimized; })){
-  while (true){
+  while (true) {
     std::cout << "\ntarget: " << target_node << "\n";
 
     // nodesのコストと比較して小さかったら更新する
     VectorWrapper(edges)
-      .filter([=](DpEdge e) { 
-        return std::find(e.nodes.begin(), e.nodes.end(), target_node) != e.nodes.end();
+      .filter([=](DpEdge e) {
+        return std::find(
+          e.nodes.begin(),
+          e.nodes.end(),
+          target_node) != e.nodes.end();
       })
-      .foreach([&](DpEdge e) { 
+      .foreach([&](DpEdge e) {
         int key = VectorWrapper(e.nodes)
           .filter([=](int x) { return x != target_node;})
           .raw_value[0];
@@ -135,38 +130,37 @@ int main(int argc, char const *argv[])
 
         std::cout << "key: " + std::to_string(key) << "\n";
         std::cout << "calc_cost: " + std::to_string(calc_cost) << "\n";
-        
+
         if (calc_cost < nodes[key].cost) {
           nodes[key].cost = calc_cost;
         }
       });
 
-    // isMinimized: falseの中で最小コストのnodeを見つけ、target_nodeにする & isMinimizedをtrue
+    // 未最適化の中で最小コストのnodeを見つけ、target_nodeにするisMinimizedをtrue
     std::unordered_map<int, DpNode> filtered;
     std::copy_if(
       nodes.begin(),
       nodes.end(),
       std::inserter(filtered, filtered.end()),
-      [](std::pair<int, DpNode>  const& node_pair){ return node_pair.second.isMinimized == false; }
-    );
+      [](std::pair<int, DpNode> const& node_pair){
+        return node_pair.second.isMinimized == false;
+      });
     auto min = std::min_element(
       filtered.begin(),
       filtered.end(),
       [](const auto &lhs, const auto &rhs) {
         return lhs.second.cost < rhs.second.cost;
-      }
-    );
+      });
 
     target_node = min->first;
     nodes[target_node].isMinimized = true;
-    if(filtered.size() == 1) {
+    if (filtered.size() == 1) {
       break;
     }
   }
 
   std::cout << "\n";
-  for(auto&& node : nodes)
-  {
+  for (auto&& node : nodes) {
     std::cout << "node: "
               << std::to_string(node.first) + ", cost: "
               << node.second.cost << "\n";
